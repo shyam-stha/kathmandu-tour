0dfeb50ed008de89117a37d076fc3dc4d8641c00
Shyam Stha <sthask2019@gmail.com>
2023-01-04 15:57:28 +0545

    Feat : Add background color and change color of text when scroll

--- src/components/partial/Navbar.tsx
+++ src/components/partial/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinkItems = [
     { label: 'Blogs', path: '/blogs' },
 ]
 
-const NavBar = (props: any) => {
+const NavBar = ({ isChange }: any) => {
     const links = navLinkItems.map((link) => {
         return (
             <NavLink
@@ -20,23 +20,35 @@ const NavBar = (props: any) => {
     })
 
     return (
-        <div className='container flex h-[90px] items-center justify-between px-[80px] '>
+        <div
+            className={`container fixed z-50 flex h-[90px] w-[100vw] items-center justify-between px-[80px] text-[#FCFCFC] ${
+                isChange ? 'bg-gray-900/10 font-bold text-black' : ''
+            }`}>
             <div className='shrink-0'>
-                <Logo type='colored' />
+                <Logo type={isChange ? 'black' : 'colored'} />
             </div>
-            <div className=' hidden h-[55px] w-[535px] cursor-text items-center rounded-[8px] bg-[#EFF0F633] py-[8px] px-[24px] md:hidden xl:flex'>
+            <div
+                className={` hidden h-[55px] w-[535px] cursor-text items-center rounded-[8px] ${
+                    isChange ? ' bg-[#eff0f686]' : ' bg-[#EFF0F633]'
+                } py-[8px] px-[24px] md:hidden xl:flex`}>
                 <img
                     src={Search}
                     alt='Search'
-                    className='h-[20px] w-[20px] text-[#EFF0F6]'
+                    className={`h-[20px] w-[20px] ${
+                        isChange ? 'text-gray-700' : ''
+                    } `}
                 />
-                <input  
+                <input
                     type='text'
                     placeholder='Search'
-                    className='ml-[16px] w-full bg-transparent text-white placeholder:text-[#D9DBE9] focus:outline-none'
+                    className={`ml-[16px] w-full bg-transparent  ${
+                        isChange
+                            ? 'placeholder:text-gray-700'
+                            : 'placeholder:text-[#D9DBE9]'
+                    } focus:outline-none`}
                 />
             </div>
-            <div className='flex gap-[40px] text-[#FCFCFC]'>{links}</div>
+            <div className='flex gap-[40px]'>{links}</div>
         </div>
     )
 }
