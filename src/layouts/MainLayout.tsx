7b2a20e77de73b509b49de69a0e9ad27d7c6cd2a
Shyam Stha <sthask2019@gmail.com>
2023-01-04 15:59:21 +0545

    Create layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LandingPage from '../pages/landing/LandingPage'
+
+type Props = {}
+
+const MainLayout = (props: Props) => {
+  return (
+    <div>
+        <LandingPage />
+    </div>
+  )
+}
+
+export default MainLayout
