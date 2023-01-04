4c0d2e9dccf4e5087bc4fa0f91cb45b616cbf146
Shyam Stha <sthask2019@gmail.com>
2023-01-04 13:57:58 +0545

    Create small card blog component

---
+++ src/components/common/CardBlogSmall.tsx
@@ -0,0 +1,21 @@
+import hero from '../../assets/images/landing/hero.png'
+
+const CardBlogsSmall = () => {
+    return (
+        <div className='relative h-[245px] w-[450px]  overflow-hidden rounded-md '>
+            <img
+                src={hero}
+                alt='image'
+                className='absolute inset-0 h-full w-full object-fill'
+            />
+            <div className='absolute bottom-0 h-[100px] w-full p-[10px] font-sans text-white backdrop-blur-[3px] '>
+                <h2 className='text-[24px] leading-6 tracking-[1px]'>
+                    Walks Local Market
+                </h2>
+                <span className='text-[20px] tracking-[0.75px]'>Dec 15</span>
+            </div>
+        </div>
+    )
+}
+
+export default CardBlogsSmall
