e56aa2edca2d04c3806d169a8da1636be0817d16
Shyam Stha <sthask2019@gmail.com>
2023-01-04 15:55:34 +0545

    Feat : Change navbar color when scroll

--- src/components/container/Hero.tsx
+++ src/components/container/Hero.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react'
 import NavBar from '../partial/Navbar'
 
 type Props = {}
 
 const Hero = (props: Props) => {
+    const [isChange, setIsChange] = useState(false)
+    function changeNavbar() {
+        if (window.scrollY >= 580) {
+            setIsChange(true)
+        } else {
+            setIsChange(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNavbar)
+    }, [])
+
     return (
-        <div className='bg-hero h-screen bg-cover bg-scroll bg-center bg-no-repeat'>
-            <NavBar />
+        <div className='flex h-screen justify-center  bg-hero bg-cover bg-center bg-no-repeat'>
+            <NavBar isChange={isChange} />
         </div>
     )
 }
