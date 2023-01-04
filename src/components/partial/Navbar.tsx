c4eb11c9ef7b215bac5a7a28a1a0e268549353da
Shyam Stha <sthask2019@gmail.com>
2023-01-04 13:54:02 +0545

    Add responsive style to navbar

--- src/components/partial/Navbar.tsx
+++ src/components/partial/Navbar.tsx
@@ -1,5 +1,5 @@
 import Logo from './Logo'
-import Search from '../../assets/images/Search.svg'
+import Search from '../../assets/images/nav/Search.svg'
 import { NavLink } from 'react-router-dom'
 
 const navLinkItems = [
@@ -10,21 +10,27 @@ const navLinkItems = [
 
 const NavBar = (props: any) => {
     const links = navLinkItems.map((link) => {
-        return <NavLink to={link.path}>{link.label}</NavLink>
+        return (
+            <NavLink
+                to={link.path}
+                key={link.label}>
+                {link.label}
+            </NavLink>
+        )
     })
 
     return (
-        <div className='container flex h-[90px] items-center justify-between px-[80px]'>
-            <div className=''>
+        <div className='container flex h-[90px] items-center justify-between px-[80px] '>
+            <div className='shrink-0'>
                 <Logo type='colored' />
             </div>
-            <div className='flex h-[55px] w-[535px] cursor-text items-center rounded-[8px] bg-[#EFF0F633] py-[8px] px-[24px]'>
+            <div className=' hidden h-[55px] w-[535px] cursor-text items-center rounded-[8px] bg-[#EFF0F633] py-[8px] px-[24px] md:hidden xl:flex'>
                 <img
                     src={Search}
                     alt='Search'
                     className='h-[20px] w-[20px] text-[#EFF0F6]'
                 />
-                <input
+                <input  
                     type='text'
                     placeholder='Search'
                     className='ml-[16px] w-full bg-transparent text-white placeholder:text-[#D9DBE9] focus:outline-none'
