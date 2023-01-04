ec09fd4c17b49cab61bd166b6adfb8a3dadfb060
Shyam Stha <sthask2019@gmail.com>
2023-01-04 14:53:59 +0545

    Create popular package component

--- src/components/container/PopularPackage.tsx
+++ src/components/container/PopularPackage.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
 import CardPackage from '../common/CardPackage'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
 const PopularPackage = (props: Props) => {
-  return (
-    <CardPackage />
-  )
+    return (
+        <div className='mt-[50px] shrink-0'>
+            <div className='mb-[30px] flex h-10 items-center justify-between font-sans'>
+                <h1 className='Font-sans text-[32px] font-bold leading-[36px] tracking-[1px]'>
+                    Popular Experiences
+                </h1>
+                <NavLink
+                    to='/'
+                    className='Font-sans text-[20px] leading-[24px] tracking-[0.75px]'>
+                    All things to do
+                </NavLink>
+            </div>
+            <div className='flex flex-wrap justify-between'>
+                <CardPackage />
+                <CardPackage />
+                <CardPackage />
+            </div>
+        </div>
+    )
 }
 
 export default PopularPackage
