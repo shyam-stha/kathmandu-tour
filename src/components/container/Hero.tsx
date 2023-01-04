de776e36635da6e8b19f36ef0c2860f400f43cba
Shyam Stha <sthask2019@gmail.com>
2023-01-04 14:03:34 +0545

    Create hero section component

---
+++ src/components/container/Hero.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../partial/Navbar'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+    return (
+        <div className='bg-hero h-screen w-screen bg-cover bg-scroll bg-center bg-no-repeat'>
+            <NavBar />
+        </div>
+    )
+}
+
+export default Hero
