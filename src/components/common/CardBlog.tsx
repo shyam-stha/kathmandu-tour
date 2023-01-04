ed9fff8aa4274e93e1bf65d58b2418f3e6426cf8
Shyam Stha <sthask2019@gmail.com>
2023-01-04 13:56:46 +0545

    Create card blog component

---
+++ src/components/common/CardBlog.tsx
@@ -0,0 +1,25 @@
+import hero from '../../assets/images/landing/hero.png'
+
+const CardBlog = () => {
+    return (
+        <div className='relative h-[500px] w-[880px]  overflow-hidden rounded-md font-sans '>
+            <img
+                src={hero}
+                alt='image'
+                className='absolute inset-0 h-full w-full object-fill'
+            />
+            <div className='absolute bottom-[40px] left-[30px] w-full p-[10px] font-sans tracking-[1px] text-white '>
+                <h2 className='text-[32px] font-bold'>
+                    Sunset Walk at Swyambhu
+                </h2>
+                <div className='flex gap-3 text-[24px] tracking-[.75px]'>
+                    <span>Written by</span>
+                    <span> Prabhat Khadka</span>
+                </div>
+                <span className='text-[20px] tracking-[0.75px]'>Dec 15</span>
+            </div>
+        </div>
+    )
+}
+
+export default CardBlog
