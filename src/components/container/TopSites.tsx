02371d40ce3952b9140d6c32af4e4f6b8c03eb52
Shyam Stha <sthask2019@gmail.com>
2023-01-04 14:55:27 +0545

    Create top visit sites and styles

--- src/components/container/TopSites.tsx
+++ src/components/container/TopSites.tsx
@@ -1,14 +1,30 @@
-import CardTopSite from "../common/CardTopSite"
-import DarbarSqr from "../../assets/images/cards/darbar_sqr.jpg"
+import CardTopSite from '../common/CardTopSite'
+import DarbarSqr from '../../assets/images/cards/darbar_sqr.jpg'
 
 type Props = {}
 
 const TopSites = (props: Props) => {
-  return (
-    <div>
-        <CardTopSite image={DarbarSqr} title="Kathmandu Darbar Square" />
-    </div>
-  )
+    return (
+        <div className='mt-[100px]'>
+            <h1 className='Font-sans mb-[30px] text-[32px] font-bold leading-[36px] tracking-[1px]'>
+                Top Sites to visit
+            </h1>
+            <div className='flex flex-wrap justify-between'>
+                <CardTopSite
+                    image={DarbarSqr}
+                    title='Kathmandu Darbar Square'
+                />
+                <CardTopSite
+                    image={DarbarSqr}
+                    title='Kathmandu Darbar Square'
+                />
+                <CardTopSite
+                    image={DarbarSqr}
+                    title='Kathmandu Darbar Square'
+                />
+            </div>
+        </div>
+    )
 }
 
 export default TopSites
