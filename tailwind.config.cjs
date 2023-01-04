5d0dbbaa3f3d67f68dab5b0026a6e6ffe1d0e3c2
Shyam Stha <sthask2019@gmail.com>
2023-01-04 12:46:40 +0545

    Extends fontFamily and backgroundImage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                hero: "url('./src/assets/images/landing/hero.png')",
+            },
+            fontFamily: {
+                roboto: 'Roboto',
+                sans: ['Helvetica', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+}
