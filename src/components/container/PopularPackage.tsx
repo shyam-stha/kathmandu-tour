ef6a6da0323adb829c99905a8674338293bcdc1c
Shyam Stha <sthask2019@gmail.com>
2023-01-04 14:04:04 +0545

    Create Popular package card component

---
+++ src/components/container/PopularPackage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardPackage from '../common/CardPackage'
+
+type Props = {}
+
+const PopularPackage = (props: Props) => {
+  return (
+    <CardPackage />
+  )
+}
+
+export default PopularPackage
