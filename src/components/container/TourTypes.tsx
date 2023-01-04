b980e25fe51437582e3730f336851be4b9255433
Shyam Stha <sthask2019@gmail.com>
2023-01-04 14:56:21 +0545

    Create and add styling to tour type componet

--- src/components/container/TourTypes.tsx
+++ src/components/container/TourTypes.tsx
@@ -5,10 +5,37 @@ type Props = {}
 
 const TourTypes = (props: Props) => {
     return (
-        <CardTourType
-            image={bikeRide}
-            title='Bike Tour'
-        />
+        <div className='mt-[100px]'>
+            <h1 className='Font-sans mb-[30px] text-[32px] font-bold leading-[36px] tracking-[1px]'>
+                Tour Types
+            </h1>
+        <div className='flex flex-wrap justify-between'>
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+                <CardTourType
+                    image={bikeRide}
+                    title='Bike Tour'
+                />
+            </div>
+        </div>
     )
 }
 
